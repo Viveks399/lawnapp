874d1dd7ed0049cac4d2104b96d28030b74dbd36
Viveks399 <vivek.me.619@gmail.com>
2024-09-09 15:55:07 +0530

    Updated navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -8,31 +8,36 @@ const Navbar = ({ scrollToService, scrollToProperties, scrollToInquire }) => {
     navigate("/career");
   };
   return (
-    <div className="w-full h-20 px-5 flex flex-col md:flex-row items-center justify-between">
+    <div className="w-full px-5 flex flex-col md:flex-row items-center justify-between">
       <img className="h-16 md:h-20 w-auto rounded-lg" src={logo2} alt="Logo" />
-      <div className="mt-4 md:mt-0 flex items-center">
+      <div className="mt-4 md:mt-0 flex flex-col md:flex-row items-center">
         <button
           className="px-4 py-2 text-sm md:text-base"
           onClick={scrollToService}
         >
           Our Service
         </button>
-        <span className="border-l border-gray-300 h-6 mx-2"></span>
+        <span className="border-l border-gray-300 h-6 mx-2 hidden md:inline-block"></span>
         <button
           className="px-4 py-2 text-sm md:text-base"
           onClick={scrollToProperties}
         >
           Properties
         </button>
-        <span className="border-l border-gray-300 h-6 mx-2"></span>
+        <span className="border-l border-gray-300 h-6 mx-2 hidden md:inline-block"></span>
         <button
           className="px-4 py-2 text-sm md:text-base"
           onClick={scrollToInquire}
         >
           Contact Us
         </button>
-        <span className="border-l border-gray-300 h-6 mx-2"></span>
-        <button className="px-4 py-2 text-sm md:text-base" onClick={handleClickCareer}>Careers</button>
+        <span className="border-l border-gray-300 h-6 mx-2 hidden md:inline-block"></span>
+        <button
+          className="px-4 py-2 text-sm md:text-base"
+          onClick={handleClickCareer}
+        >
+          Careers
+        </button>
       </div>
       <div className="mt-4 md:mt-0">
         <button
